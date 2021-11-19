2f589a33d89ba689ea397a8bf1e345df9f2d3c14
u17207067 <u17207067@utp.edu.pe>
2021-11-18 23:20:12 -0500

    agregando el homeActivity

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.afinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth nfirebaseAuth;
+    private FirebaseAuth.AuthStateListener mautListener;
+    public static final int SGN_IN =1;
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.GoogleBuilder.().build()
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfirebaseAuth = FirebaseAuth.getInstance();
+        mautListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    vamosahome();
+                }
+            }
+
+            private void vamosahome() {
+            }
+        };
+
+
     }
 }
